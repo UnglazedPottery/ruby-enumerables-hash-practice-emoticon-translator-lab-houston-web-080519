46ed6fa664c0cc7d724445a6b308fe64de3f6c9b
Jade Huynh <35jadeh@gmail.com>
2019-07-29 17:00:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 def load_library(file_path)
   library = YAML.load_file(file_path)
-  emoji_hash = {}
+  emoji_hash = {"get meaning"}
   library.each do |meaning, emoticon|
     
   end
