5350bd29fd24c7591db9dadc331fcb8befefa643
Jade Huynh <35jadeh@gmail.com>
2019-07-29 17:50:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ end
 def get_japanese_emoticon(file_path, english_emoticon)
   library = load_libary(file_path)
   if library["get_emoticon"].include?(english_emoticon)
-    library["get_emoticon"]["english_emoticon"]
+    library["get_emoticon"][english_emoticon]
   else
   end
 end
