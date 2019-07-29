3630853f22204adcc49296a9ed65eaec9396d7ea
Jade Huynh <35jadeh@gmail.com>
2019-07-29 18:10:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_japanese_emoticon(file_path, english_emoticon)
   if library["get_emoticon"].include?(english_emoticon)
     library["get_emoticon"][english_emoticon]
   else
-    "English emoticon not found."
+    "Sorry, that emoticon was not found"
   end
 end
 
@@ -24,6 +24,6 @@ def get_english_meaning(file_path, japanese_emoticon)
   if library["get_meaning"].include?(japanese_emoticon)
     library["get_meaning"][japanese_emoticon]
   else
-    "Japanese emoticon not found."
+    "Sorry, that emoticon was not found"
   end
 end
