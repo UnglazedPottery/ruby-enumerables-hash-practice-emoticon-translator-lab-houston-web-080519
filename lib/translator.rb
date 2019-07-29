fe15c802749d42547bf2660c7ee787948cd7c50f
Jade Huynh <35jadeh@gmail.com>
2019-07-29 16:58:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,9 @@ require "yaml"
 
 def load_library(file_path)
   emoji_library = YAML.load_file(file_path)
-  emoji_library.each do ||
+  emoji_library.each do |meaning, emoticon|
+    
+  end
 end
 
 def get_japanese_emoticon(file_path, emoticon)
